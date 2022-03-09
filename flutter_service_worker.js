ef0a9987d616000d884b4ab510ae758eebc8f2dd
Tor Smedberg <tor.smedberg@gmail.com>
2022-03-09 09:40:06 +0100

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -17,8 +17,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "da733379d4116c86d0331744e57c8cca",
-"/": "da733379d4116c86d0331744e57c8cca",
+"index.html": "03d5483fa118fcd2c37f50d948cc455b",
+"/": "03d5483fa118fcd2c37f50d948cc455b",
 "main.dart.js": "08b0a38c20911cdfbd20fad3eddf8389",
 "manifest.json": "a9a53f63231e292e16a4861b958274b7",
 "version.json": "962b9e99c6fc32608e4953327a24433e"
